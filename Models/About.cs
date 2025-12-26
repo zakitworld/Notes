2de9da539e23c38fa2c740b4c4a07cd2af54bec1
zakitworld <zakitworld@gmail.com>
2025-12-25 16:21:18 -1200

    Refactor: Restructure project with Views folder and data binding models
    - Move AboutPage and related code to Views folder with updated namespace
    - Create AllNotesPage for displaying all notes with CollectionView binding
    - Create NotePage for individual note editing with save/delete functionality
    - Add About, AllNotes, and Note data model classes in Models folder
    - Implement data binding using MVVM pattern for About page
    - Update AppShell routing to reference Views namespace and register NotePage route
    - Update project file references to point to Views folder location
    - Remove old MainPage and template code

---
+++ Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://zakitworld.netlify.app";
+    public string Message => "This app is written by Abdul-Razak of Zak IT World in XAML and C# with .NET MAUI.";
+}
